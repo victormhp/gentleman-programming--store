401abefad3478d2dfe9d5498760d29b477a511cc
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-12 16:15:38 -0600

    Add app

--- interview-1/src/App.jsx
+++ interview-1/src/App.jsx
@@ -1,5 +1,28 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Header, Footer, Progress } from './components';
+import { CartProvider } from './context/cart';
+
+const Home = lazy(() => import('./pages/Home'));
+const ProductsPage = lazy(() => import('./pages/ProductsPage'));
+const About = lazy(() => import('./pages/About'));
+const CartPage = lazy(() => import('./pages/CartPage'));
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Suspense fallback={<Progress />}>
+      <CartProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<ProductsPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/cart' element={<CartPage />} />
+        </Routes>
+        <Footer />
+      </CartProvider>
+    </Suspense>
+  );
 }
 
 export default App;
