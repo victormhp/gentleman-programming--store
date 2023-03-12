47acd63a965ab6f793f2badfd5841ebe23fc70c0
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-12 16:15:11 -0600

    Ease the export of components

---
+++ interview-1/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Progress } from './Progress/Progress';
+export { default as Header } from './Header/Header';
+export { default as Filters } from './Filters/Filters';
+export { default as ProductsList } from './ProductsList/ProductsList';
+export { default as Cart } from './Cart/Cart';
+export { default as Footer } from './Footer/Footer';
