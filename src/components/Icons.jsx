32627653fbb45c13f29e6aa748911067e258c024
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-13 21:00:00 -0600

    Add icon as a component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -108,3 +108,14 @@ export function ClearCartIcon() {
     </svg>
   );
 }
+
+export function SearchIcon() {
+  return (
+    <svg width='16' height='16' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g stroke='#fff' strokeWidth={2}>
+        <path d='m17 17 4 4' strokeLinecap='round' strokeLinejoin='round' />
+        <path d='M19 11a8 8 0 1 1-16 0 8 8 0 0 1 16 0Z' />
+      </g>
+    </svg>
+  );
+}
