f57187b6cfc407ff229910f0eedeaf3c930c0e53
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-16 19:06:33 -0600

    Change Progress component name to Loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.css';
+
+function Loading() {
+  return (
+    <div className='loading'>
+      <div className='loading-circle'></div>
+    </div>
+  );
+}
+
+export default Loading;
