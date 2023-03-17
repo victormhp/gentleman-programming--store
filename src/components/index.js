7fef1c93c7a5b7d2d5227e2f4e307e4c688f50cc
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-16 23:40:20 -0600

    Add ErrorPage

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as Header } from './Header/Header';
 export { default as Search } from './Search/Search';
 export { default as Filters } from './Filters/Filters';
 export { Products, ProductPage } from './Products';
-export { default as Cart } from './Cart';
+export { default as CartList } from './Cart';
 export { default as Footer } from './Footer/Footer';
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,32 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import { Root, Home, Products, Cart, Error } from './routes';
+import { Loading, ProductPage } from './components';
 import { FiltersProvider } from './context/filters';
+import { CartProvider } from './context/cart';
 
-import App from './App';
 import './index.css';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />} errorElement={<Error />}>
+      <Route path='/' element={<Home />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/products/:name' element={<ProductPage />}></Route>
+      <Route path='/cart' element={<Cart />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <FiltersProvider>
-      <App />
-    </FiltersProvider>
-  </BrowserRouter>
+  <FiltersProvider>
+    <CartProvider>
+      <RouterProvider router={router} fallbackElement={<Loading />} />
+    </CartProvider>
+  </FiltersProvider>
 );
