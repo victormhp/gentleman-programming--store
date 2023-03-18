3394ec91d0cab39713cb40b16a8ceb0d978e8cf2
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-17 18:37:39 -0600

    Add lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -5,28 +5,31 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
-import { Root, Home, Products, Cart, Error } from './routes';
-import { Loading, ProductPage } from './components';
-import { FiltersProvider } from './context/filters';
-import { CartProvider } from './context/cart';
-
+import { Loading } from './components';
+import { Suspense, lazy } from 'react';
+import { productsLoader } from './services/productsLoader';
 import './index.css';
+
+const Root = lazy(() => import('./routes/Root/Root'));
+const Error = lazy(() => import('./routes/Error/Error'));
+const Home = lazy(() => import('./routes/Home/Home'));
+const Main = lazy(() => import('./routes/Main/Main'));
+const ProductPage = lazy(() => import('./routes/ProductPage/ProductPage'));
+const Cart = lazy(() => import('./routes/Cart/Cart'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Root />} errorElement={<Error />}>
+    <Route path='/' element={<Root />} loader={productsLoader} errorElement={<Error />}>
       <Route path='/' element={<Home />} />
-      <Route path='/products' element={<Products />} />
-      <Route path='/products/:name' element={<ProductPage />}></Route>
+      <Route path='/products' element={<Main />} loader={productsLoader} />
+      <Route path='/products/:name' element={<ProductPage />} loader={productsLoader} />
       <Route path='/cart' element={<Cart />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <FiltersProvider>
-    <CartProvider>
-      <RouterProvider router={router} fallbackElement={<Loading />} />
-    </CartProvider>
-  </FiltersProvider>
+  <Suspense fallback={<Loading />}>
+    <RouterProvider router={router} />
+  </Suspense>
 );
