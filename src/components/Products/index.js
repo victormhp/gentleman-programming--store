ec1dbdf1208a884bc008c1ce2b4632e28352fa4e
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-17 18:37:29 -0600

    Change exports

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,2 +1,1 @@
 export { default as Products } from './ProductsList';
-export { default as ProductPage } from './ProductPage';
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 export { default as Loading } from './Loading/Loading';
+export { AddToCart, Quantity } from './Buttons';
 export { default as Header } from './Header/Header';
 export { default as Search } from './Search/Search';
 export { default as Filters } from './Filters/Filters';
-export { Products, ProductPage } from './Products';
+export { Products } from './Products';
 export { default as CartList } from './Cart';
 export { default as Footer } from './Footer/Footer';
