a16a077b7dbef0d752ef46356cecfbe79706bda6
Victor Alberto <victoralbertomhp@gmail.com>
2023-03-19 21:49:18 -0600

    Update main app and component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
 export { default as Loading } from './Loading/Loading';
-export { AddToCart, Quantity } from './Buttons';
 export { default as Header } from './Header/Header';
-export { default as Search } from './Search/Search';
 export { default as Filters } from './Filters/Filters';
-export { Products } from './Products';
+export { default as ProductsList } from './Products';
 export { default as CartList } from './Cart';
 export { default as Footer } from './Footer/Footer';
--- src/main.jsx
+++ src/main.jsx
@@ -13,15 +13,15 @@ import './index.css';
 const Root = lazy(() => import('./routes/Root/Root'));
 const Error = lazy(() => import('./routes/Error/Error'));
 const Home = lazy(() => import('./routes/Home/Home'));
-const Main = lazy(() => import('./routes/Main/Main'));
-const ProductPage = lazy(() => import('./routes/ProductPage/ProductPage'));
-const Cart = lazy(() => import('./routes/Cart/Cart'));
+const Products = lazy(() => import('./routes/Products/Products'));
+const ProductPage = lazy(() => import('./components/Products/ProductPage/ProductPage'));
+const Cart = lazy(() => import('./components/Cart/CartList/CartList'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Root />} loader={productsLoader} errorElement={<Error />}>
       <Route path='/' element={<Home />} />
-      <Route path='/products' element={<Main />} loader={productsLoader} />
+      <Route path='/products' element={<Products />} loader={productsLoader} />
       <Route path='/products/:name' element={<ProductPage />} loader={productsLoader} />
       <Route path='/cart' element={<Cart />} />
     </Route>
